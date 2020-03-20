1ce2a5377fed291a11b50f6aa7fc5971937878ad
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-03-20 23:07:43 +0400

    modified form:

--- js/script.js
+++ js/script.js
@@ -3,9 +3,18 @@ var modal = document.querySelector(".form-search");
 var form = document.querySelector("form");
 var date = document.querySelector(".date");
 
-trigger.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    modal.classList.toggle("form-search-hide");
+modal.classList.add("form-search-hide");
+
+trigger.addEventListener("click", function() {
+    if (modal.classList.contains("form-search-hide")) {
+        modal.classList.remove("animated-hide");
+        modal.classList.remove("form-search-hide");
+        modal.classList.add("animated-add");
+    } else {
+        modal.classList.remove("animated-add");
+        modal.classList.add("form-search-hide");
+        modal.classList.add("animated-hide");
+    }
 });
 
 form.addEventListener("submit", function(evt) {
