a0a31938cb71e25507fbf6637b8b4f2a36e196de
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-03-20 21:09:31 +0400

    modified filter and amimated form

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var trigger = document.querySelector(".button-trigger");
+var modal = document.querySelector(".form-search");
+var form = document.querySelector("form");
+var date = document.querySelector(".date");
+
+trigger.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.toggle("form-search-show");
+});
+
+form.addEventListener("submit", function(evt) {
+    if (!date.value) {
+        evt.preventDefault();
+    } 
+});
